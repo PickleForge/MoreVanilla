eb62fedbbf1f53a1a6c4f07fbcf00191eda63d35
Ethan Manzi <manziethan11@gmail.com>
2020-09-09 02:21:18 -0400

    Deleted a file accidently before commiting!

--- src/main/java/com/amazedkeys/pickleforge/morevanilla/MoreVanilla.java
+++ src/main/java/com/amazedkeys/pickleforge/morevanilla/MoreVanilla.java
@@ -1,8 +1,15 @@
 package com.amazedkeys.pickleforge.morevanilla;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+
+import java.rmi.registry.Registry;
 
 public class MoreVanilla implements ModInitializer {
+
+	public static final Item EXAMPLE_ITEM = new Item(new Item.Settings().group(ItemGroup.MISC));
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -10,5 +17,8 @@ public class MoreVanilla implements ModInitializer {
 		// Proceed with mild caution.
 
 		System.out.println("Loading MoreVanilla");
+
+		// Register items
+		Registry<T>.register(Registry.ITEM, new Identifier(""), object_1);
 	}
 }
